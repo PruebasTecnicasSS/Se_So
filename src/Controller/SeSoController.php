b94d2f5129abd477e435bf6423fb629175d0e0f7
root <siguaraya@gmail.com>
2019-11-23 11:06:26 +0000

    Controller methods

--- src/Controller/SeSoController.php
+++ src/Controller/SeSoController.php
@@ -59,23 +59,28 @@ class SeSoController extends Controller
     public function bookmarksAction(Request $request, basicORM $ORM, EntityManagerInterface $entityManager)
     {
 
-        $aBookmarks  = $ORM->getBookmarks($entityManager);
+        $aBookmarks = $ORM->getBookmarks($entityManager);
 
-        return $this->render('/bookmarks.html.twig', array('bookmarks' => $aBookmarks));
+
+
+        return $this->render('/bookmarks.html.twig', array('data' => $aBookmarks));
     }
 
-
     /**
-     * @Route("/editUrl", name="edit_url")
+     * @Route("/addUrl", name="add_url")
      *
      */
-    public function editUrl(Request $request, basicORM $ORM, EntityManagerInterface $entityManager)
+    public function addUrlAction(Request $request, basicORM $ORM, EntityManagerInterface $entityManager)
     {
-
+        //get all request
         $aUrlParam = $request->request->all();
-        $sError = '';
+        $sError    = '';
 
-        $bSaveUrl = $ORM->saveUrl($entityManager, $aUrlParam, $sError);
+        //get param
+        $sUrl      = $aUrlParam['url'];
+
+        //save data
+        $bSaveUrl = $ORM->saveUrl($entityManager, $sUrl, $sError);
 
         if ($bSaveUrl) {
             return new Response('OK', Response::HTTP_OK);
@@ -85,26 +90,67 @@ class SeSoController extends Controller
     }
 
 
+    /**
+     * @Route("/editUrl", name="edit_url")
+     *
+     */
+    public function editUrlAction(Request $request, basicORM $ORM, EntityManagerInterface $entityManager)
+    {
+        //get all request
+        $aUrlParam = $request->request->all();
+        $sError    = '';
+
+        //get param
+        $sUrl      = $aUrlParam['url'];
+        $nIdUrl    = $aUrlParam['id'];
+
+        //save data
+        $bSaveUrl = $ORM->editUrl($entityManager, $sUrl, $nIdUrl, $sError);
+
+        if ($bSaveUrl) {
+            return new Response('OK', Response::HTTP_OK);
+        } else {
+            return new Response($sError, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @Route("/delete", name="delete_url")
+     *
+     */
+    public function deleteAction(Request $request, basicORM $ORM)
+    {
+        $nIdUrl = $request->get('id', null);
+
+        $bDelete = $ORM->deleteUrl($this->entityManagerCustomer(), $nIdUrl, $sError);
+
+        if ($bDelete) {
+            return new Response('OK', Response::HTTP_OK);
+        } else {
+            return new Response($sError, Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
     /**
      * @param $url
      * @return string
      */
-    private function twitterling($url){
+    private function twitterling($url)
+    {
 
-        $title = 'Title here';
+        $title     = 'Title here';
         $short_url = 'http://shorturl.co';
-        $url = 'http://fullurl.com';
+        $url       = 'http://fullurl.com';
 
         $twitter_params =
-            '?text=' . urlencode($title) . '+-' .
-            '&amp;url=' . urlencode($short_url) .
-            '&amp;counturl=' . urlencode($url) .
+            '?text='.urlencode($title).'+-'.
+            '&amp;url='.urlencode($short_url).
+            '&amp;counturl='.urlencode($url).
             '';
 
 
-        $link = "http://twitter.com/share" . $twitter_params . "";
+        $link = "http://twitter.com/share".$twitter_params."";
 
         return $link;
     }
