e7aeaafc3acb66a8f64264fe44752b756e55bb12
root <siguaraya@gmail.com>
2019-11-23 12:21:26 +0000

    Export data to XML

--- src/Controller/SeSoController.php
+++ src/Controller/SeSoController.php
@@ -99,4 +99,49 @@ class SeSoController extends Controller
     }
 
 
+    /**
+     * @Route("/exportXML", name="export_url")
+     */
+    public function exportXML(Request $request, basicORM $ORM, EntityManagerInterface $entityManager)
+     {
+        //get urls fromm db
+         $aBookmarks = $ORM->getBookmarks($entityManager);
+
+         //create a XMl object
+         $objetoXML = new \XMLWriter();
+
+         //configure XML
+         $objetoXML->openMemory();
+         $objetoXML->setIndent(true);
+         $objetoXML->setIndentString("\t");
+         $objetoXML->startDocument('1.0', 'ISO-8859-1');
+
+         //begin to save data
+         $objetoXML->startElement('BOOKMARKS');
+         foreach ($aBookmarks as $bookmark) {
+             $objetoXML->startElement('URL');
+             $objetoXML->text($bookmark->getUrl());
+             $objetoXML->endElement();
+             $objetoXML->startElement('DATE');
+             $objetoXML->text($bookmark->getDate());
+             $objetoXML->endElement();
+         }
+        //end save data
+         $objetoXML->endElement();//
+
+         //end document
+         $objetoXML->endDocument();
+
+         //get XML into string
+         $XML = trim($objetoXML->outputMemory());
+
+         //create response
+         $response = new Response($XML);
+         $response->headers->set('Content-Type', 'xml');
+
+         return $response;
+
+    }
+
+
 }
